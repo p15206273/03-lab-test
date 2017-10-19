2a2a0d384876b6f8b77882d6649c24435df22157
Kacper Kozak <P15206273@LEC-ADMIN.dmu.ac.uk>
2017-10-19 16:38:37 +0100

    added header to html and js

--- js/greeting.js
+++ js/greeting.js
@@ -3,9 +3,13 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
-
+  let name = document.getElementById("name");
+  
   function PerformGreeting(ev) {
     myName.innerHTML = userName.value;
+	
+	name.innerHTML = userName.value;
+
     ev.preventDefault();
   }
 
